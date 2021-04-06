9f828663f46785a4e18ece603876d4df2b209502
ErikModica <erikmodica@gmail.com>
2021-04-06 16:26:52 -0700

    Figure out the structure and styling for the Home page.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,10 +3,25 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
-      <div className="navbar">
+      <header className="navbar">
         <i className="fas fa-home"></i>
         <div>WORKOUT VAULT</div>
-      </div>
+      </header>
+      <main>
+        <div className="app-drawer">
+          <div className="tile-container">
+            <div className="bg-app-tile"></div>
+            <div className="app-tile">
+              <div className="tile-title">
+                <h2>BROWSE EXERCISES</h2>
+              </div>
+              <div className="icon">
+                <i className="fas fa-search"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
