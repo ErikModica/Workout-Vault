8f83aa4ca41f40814209ae7d128aa8d906b2e0ad
ErikModica <erikmodica@gmail.com>
2021-04-06 15:53:57 -0700

    Add the basic HTML skeleton for the page and style it with CSS

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
+      <div className="navbar">
+        <i className="fas fa-home"></i>
+        <div>WORKOUT VAULT</div>
+      </div>
     </>
   );
 }
