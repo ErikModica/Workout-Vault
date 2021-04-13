3706cc0de2f76a1acca678d514201220295a74ac
ErikModica <erikmodica@gmail.com>
2021-04-13 15:39:17 -0700

    add the proper directories and files for this issue

--- client/pages/exercises.jsx
+++ client/pages/exercises.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MuscleAccordion from '../components/muscle-accordion';
-import MuscleAccordionDesktop from '../components/accordion-desktop/muscle-accordion-desktop';
+import MuscleAccordion from '../components/exercises-mobile/muscle-accordion';
+import MuscleAccordionDesktop from '../components/exercises-desktop/muscle-accordion-desktop';
 
 export default function Exercises(props) {
   return (
---
+++ client/pages/saved-exercises.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Exercises(props) {
+  return (
+    <>
+      <div className="current-page">EXERCISES</div>
+      <main>
+
+      </main>
+    </>
+  );
+}
