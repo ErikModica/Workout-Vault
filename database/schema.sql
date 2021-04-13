b92584039ab2420579ba75ee024cb375c0a6f531
ErikModica <erikmodica@gmail.com>
2021-04-13 16:38:33 -0700

    create a database that has a table for users and saved exercise ID's

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,20 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."users" (
+  "userId"         serial,
+  "username"       text           not null,
+  "password"       text           not null,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
+
+create table "public"."saved-exercises" (
+  "userId"         serial,
+  "username"       text           not null,
+  "exerciseId"     int            not null,
+  primary key ("userId"),
+  unique ("username")
+);
