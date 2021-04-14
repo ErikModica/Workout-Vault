604a1d4f83d5ef9dd0c63bfa29f0a369dea4cf7c
ErikModica <erikmodica@gmail.com>
2021-04-13 17:35:14 -0700

    in index.js add a post request that adds exercise IDs to new table

--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ require('dotenv/config');
 const nodeFetch = require('node-fetch');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+// const ClientError = require('./client-error');
+const pg = require('pg');
 
 const app = express();
 
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/workoutVault',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 app.use(staticMiddleware);
+
+app.use(express.json());
 
 app.get('/api/muscles', (req, res, next) => {
   getMuscles()
@@ -21,6 +32,30 @@ app.get('/api/exercises-by-muscle/:muscleid', (req, res, next) => {
 app.get('/api/exercise-by-id/:exerciseid', (req, res, next) => {
   getExerciseByID(req.params.exerciseid)
     .then(results => res.json(results));
+});
+
+app.post('/api/saved-exercises', (req, res, next) => {
+  const { userId, exerciseId, username } = req.body;
+  if (!userId || !exerciseId || !username) {
+    // throw new ClientError(400, 'userId and exerciseId are required fields');
+    res.status(400).json({ error: 'userId and exerciseId are required fields' });
+  }
+  const sql = `
+              insert into "saved-exercises" ("userId", "exerciseId", "username")
+              values ($1, $2, $3)
+              returning *
+              `;
+  const values = [userId, exerciseId, username];
+
+  db.query(sql, values)
+    .then(result => {
+      res.status(200).json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      // throw new ClientError(500, 'an unexpected error occured.');
+      res.status(500).json({ error: 'an unexpected error occured.' });
+    });
 });
 
 app.listen(process.env.PORT, () => {
