4db82684e620efb098e9769d5a0341b0864e5211
ErikModica <erikmodica@gmail.com>
2021-04-26 14:18:10 -0700

    added a the layout to the render functions on both mobile and desktop that render a list and app drawer of the requested user workouts from the database. also styled the workout list a bit differently

--- client/components/user-workouts/user-workouts-desktop/user-workouts-app-drawer.jsx
+++ client/components/user-workouts/user-workouts-desktop/user-workouts-app-drawer.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 
 export default class UserWorkoutsAppDrawer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { userWorkouts: [] };
+  }
+
+  componentDidMount() {
+    fetch('/api/user-workouts')
+      .then(result => result.json())
+      .then(workouts => {
+        this.setState({ userWorkouts: workouts });
+      });
+  }
+
   render() {
     return (
-      <div className="user-workouts-desktop">DESKTOP APP DRAWER OF WORKOUTS</div>
+      <div className="app-drawer user-workouts-desktop">
+        {this.state.userWorkouts.map(workout => (
+            <a className='tile-container' key={workout.workoutId}>
+              <div className='bg-app-tile'></div>
+              <div className="app-tile">
+                <div className="tile-title">
+                  <h5 exerciseid={workout.workoutId}>{workout.workoutName}</h5>
+                </div>
+              </div>
+            </a>
+        ))
+        }
+      </div>
     );
   }
 }
--- client/components/user-workouts/user-workouts-mobile/user-workouts-accordion.jsx
+++ client/components/user-workouts/user-workouts-mobile/user-workouts-accordion.jsx
@@ -3,18 +3,29 @@ import React from 'react';
 
 export default class UserWorkoutsAccordion extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { userWorkouts: [] };
+  }
+
   componentDidMount() {
     fetch('/api/user-workouts')
       .then(result => result.json())
       .then(workouts => {
-        console.log(workouts);
+        this.setState({ userWorkouts: workouts });
       });
-
   }
 
   render() {
     return (
-      <div className="user-workouts-mobile">MOBILE ACCORDION OF WORKOUTS</div>
+      <div className="workout-list-container-mobile">
+        {this.state.userWorkouts.map(workout => (
+          <div key={workout.workoutId} className="workout-list-item">
+            <a workoutid={workout.workoutId}>{workout.workoutName}</a>
+          </div>
+        ))
+        }
+      </div>
     );
   }
 }
