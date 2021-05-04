56b88fa945776de95342ab517a43206b78b511d6
ErikModica <erikmodica@gmail.com>
2021-05-03 18:10:21 -0700

    changed the reps and weight selecs to input type fields and set limmiters to them

--- client/components/create-workout-form/set-item.jsx
+++ client/components/create-workout-form/set-item.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 
 export default class SetItem extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = ({ reps: null, weight: null, alternation: null, duration: null });
+  }
+
   render() {
     return (
       <div className="set-item-container">
-        <div className="set-item-reps-weight-container">
+        <div className="set-item-reps-weight-number-container">
           <div className="exercise-select-title set-title-num">{`Set ${this.props.number}`}</div>
-            <div className="sirwc">
-              <div className="exercise-select-title">Reps</div>
-              <select className="cw-form-item" name="reps-select">
-                <option>yooyi</option>
-              </select>
-              <div className="exercise-select-title">Weight</div>
-              <select className="cw-form-item" name="weight-select">
-                <option>yooyi</option>
-              </select>
-          </div>
+          <div className="set-item-reps-weight-container">
+            <label className="exercise-select-title">Reps</label>
+            <input type="number" className="cw-form-item" max="200" name="reps-select"></input>
+            <label className="exercise-select-title">Weight</label>
+            <input type="number" className="cw-form-item" max="1000" name="weight-select"></input>
+        </div>
         </div>
       </div>
     );
