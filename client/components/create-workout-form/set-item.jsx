d12864d70f1aea88bfd4f29a76f3309125c0322e
ErikModica <erikmodica@gmail.com>
2021-05-04 18:05:18 -0700

    added a close option to the extra settings dropdown and created an onChange function that sets the state to the changed value in the appropriate field.

--- client/components/create-workout-form/set-item.jsx
+++ client/components/create-workout-form/set-item.jsx
@@ -6,18 +6,26 @@ export default class SetItem extends React.Component {
     super(props);
     this.state = ({ reps: null, weight: null, alternation: null, duration: null, extraSettingsShown: false });
     this.showExtraSettings = this.showExtraSettings.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   showExtraSettings() {
     this.setState({ extraSettingsShown: !this.state.extraSettingsShown });
   }
 
+  handleChange() {
+    const changedField = event.target.name;
+    this.setState({ [changedField]: event.target.value });
+  }
+
   renderExtraSettings() {
     return (
+      <>
       <div className="set-item-extra-settings-container">
         <div className="set-item-extra-settings-input-group">
           <div className="exercise-select-title">Alternation</div>
-          <select name="alternation-select" className="cw-form-item">
+          <select onChange={this.handleChange} name="alternation" className="cw-form-item">
+            <option value="" display="none">Select Alternation</option>
             <option value="negative">negative</option>
             <option value="bottom hold">bottom hold</option>
             <option value="mid hold">mid hold</option>
@@ -25,9 +33,11 @@ export default class SetItem extends React.Component {
           </select>
         </div>
         <label className="exercise-select-title set-item-extra-settings-input-group">Duration
-          <input type="number" className="cw-form-item" min="0" max="600" name="duration-input"></input>
+          <input onChange={this.handleChange} type="number" className="cw-form-item" min="0" max="600" name="duration"></input>
         </label>
       </div>
+        <i onClick={this.showExtraSettings} className="si-dropdown-icon fas fa-caret-up"></i>
+      </>
     );
   }
 
@@ -38,13 +48,15 @@ export default class SetItem extends React.Component {
           <div className="exercise-select-title set-title-num">{`Set ${this.props.number}`}</div>
           <div className="set-item-reps-weight-container">
             <label className="exercise-select-title">Reps</label>
-            <input type="number" className="cw-form-item" min="0" max="200"></input>
+            <input onChange={this.handleChange} type="number" className="cw-form-item" min="0" max="200" name="reps"></input>
             <label className="exercise-select-title">Weight</label>
-            <input type="number" className="cw-form-item" min="0" max="1000"></input>
+            <input onChange={this.handleChange} type="number" className="cw-form-item" min="0" max="1000" name="weight"></input>
           </div>
         </div>
         <div className="set-item-dropdown">
-          {this.state.extraSettingsShown ? this.renderExtraSettings() : <i onClick={this.showExtraSettings} className="si-dropdown-icon fas fa-caret-down"></i>}
+          {this.state.extraSettingsShown
+            ? this.renderExtraSettings()
+            : <i onClick={this.showExtraSettings} className="si-dropdown-icon fas fa-caret-down"></i>}
         </div>
       </div>
     );
